374bcd001504862e74b901428ce258d64a3196c5
Omkar <tondawalkar.omkar@gmail.com>
2017-08-05 18:57:59 +0530

    Basic BArcode Sacn and Encode

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import {BarcodeScanner,BarcodeScannerOptions} from '@ionic-native/barcode-scanner'
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,31 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+options :BarcodeScannerOptions;
+result: {};
+  constructor(private barcode:BarcodeScanner,public navCtrl: NavController) {
 
   }
 
+  async scanBarcode(){
+
+    this.options = {
+
+      prompt: 'Scan a barcode'
+    }
+
+this.result=await this.barcode.scan( this.options);
+console.log(this.result);
+  }
+
+async encodeData(){
+
+
+  await this.barcode.encode(this.barcode.Encode.TEXT_TYPE,'Omkar resting')
 }
+
+
+
+}
+
+
